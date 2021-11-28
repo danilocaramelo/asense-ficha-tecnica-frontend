57bf4160c1964f5c47d5665dccca8f5c0fcde46d
Danilo Caramelo <danilo.caramelogit@gmail.com>
2021-11-28 20:04:16 -0300

    feat: resolve home responsivity

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,96 +25,45 @@ export default function Login() {
         <MainHeader />
         <Content>
           <Row align="middle" style={{ height: 540 }}>
-            <Col span={8} offset={3}>
+            <Col span={24} lg={{ span: 8, offset: 3 }}>
               <div className="home-title">Projeto Ficha Tecnica</div>
             </Col>
-            <Col span={8} offset={2}>
+            <Col span={24} lg={{ span: 8, offset: 2 }}>
               <Row align="middle" justify="center">
                 <Link to={paths.REGISTER}>
-                  <Button
-                    style={{
-                      backgroundColor: "#EE7330",
-                      color: "#fff",
-                      borderRadius: 30,
-                      height: 63,
-                      width: 210,
-                      fontSize: 18,
-                      fontWeight: "bold",
-                      fontFamily: "Roboto Slab",
-                      marginRight: 40,
-                    }}
-                  >
-                    Cadastre-se Grátis
-                  </Button>
+                  <Button className="home-buttons">Cadastre-se Grátis</Button>
                 </Link>
                 <Link to={paths.LOGIN}>
-                  <Button
-                    style={{
-                      backgroundColor: "#EE7330",
-                      color: "#fff",
-                      borderRadius: 30,
-                      height: 63,
-                      width: 210,
-                      fontSize: 18,
-                      fontWeight: "bold",
-                      fontFamily: "Roboto Slab",
-                    }}
-                  >
-                    Entrar
-                  </Button>
+                  <Button className="home-buttons">Entrar</Button>
                 </Link>
               </Row>
             </Col>
           </Row>
-          <Row
-            align="middle"
-            justify="space-between"
-            style={{ backgroundColor: "#FC7C5F", height: 365 }}
-          >
-            <Col span={4} offset={3}>
-              <div
-                style={{
-                  fontSize: 35,
-                  fontWeight: "bold",
-                  fontFamily: "Roboto Slab",
-                  color: "#fff",
-                  marginBottom: 20,
-                }}
-              >
+          <Row align="middle" justify="space-between" className="home-footer">
+            <Col span={24} lg={{ span: 4, offset: 3 }}>
+              <div className="home-footer-highlight-text">
                 Maior controle de seus custos e lucros
               </div>
-              <div
-                style={{
-                  fontSize: 19,
-                  fontFamily: "Roboto Slab",
-                  color: "#fff",
-                }}
-              >
+              <div className="home-footer-text">
                 Com a ficha técnica você pode listar o preço de custo dos seus
                 produtos
               </div>
             </Col>
-            <Col span={5}>
-              <div
-                style={{
-                  fontSize: 20,
-                  fontFamily: "Roboto Slab",
-                  color: "#fff",
-                }}
-              >
-                * Férias pagas
-                <br /> * Serviço gratuito
-                <br /> * Cadastre quantos produtos quiser
-                <br /> * Mais controle do acesso de seus colaboradores
-                <br /> * Veja o lucro exato de cada produto
-                <br /> * Veja produtos com maior custo e benefício de venda
-              </div>
+            <Col span={24} lg={{ span: 5 }}>
+              <ul className="home-footer-list">
+                <li>Férias pagas</li>
+                <li>Serviço gratuito</li>
+                <li>Cadastre quantos produtos quiser</li>
+                <li>Mais controle do acesso de seus colaboradores</li>
+                <li>Veja o lucro exato de cada produto</li>
+                <li>Veja produtos com maior custo e benefício de venda</li>
+              </ul>
             </Col>
-            <Col>
+            <Col style={{ height: "100%" }} span={24} lg={{ span: 6 }}>
               <img
+                className="home-footer-image"
                 alt={"cartoon de varios comércios"}
                 src={footerImage}
-                style={{ height: 360, width: 550 }}
               ></img>
             </Col>
           </Row>
